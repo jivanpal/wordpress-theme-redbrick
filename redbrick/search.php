ec839957b1ca1951e3ee45c2b209f00d3fca491a
Jivan Pal <jivan.pal@gmail.com>
2019-05-24 20:03:12 +0100

    User notice that we only return 100 search results

--- redbrick/search.php
+++ redbrick/search.php
@@ -13,6 +13,9 @@
         </form>
         <?php if (have_posts()) : ?>
             <p>Found <?php echo $number_of_results; ?> results for <i><?php echo $html_query_string; ?></i>.</p>
+            <?php if ($number_of_results > 100) : ?>
+                <p>Only displaying the first 100 results.</p>
+            <?php endif; ?>
             <ul class="post-list">
                 <?php
                     while (have_posts()) {
