b71c7edf468f5391923e8629704c8d74adb3f218
Jivan Pal <jivan.pal@gmail.com>
2019-05-30 22:15:04 +0100

    Responsive footer

--- redbrick/footer.php
+++ redbrick/footer.php
@@ -1,46 +1,56 @@
         <footer>
-            <div class="social-media">
-                <h1>Connect</h1>
-                <ul>
-                    <li><a href="https://www.facebook.com/redbrickpaper/">
-                        <div class="icon-container">
-                            <?php echo file_get_contents(get_template_directory() . '/assets/facebook-icon.svg'); ?>
-                        </div>
-                    </a></li>
-                    <li><a href="https://twitter.com/redbrickpaper">
-                        <div class="icon-container">
-                            <?php echo file_get_contents(get_template_directory() . '/assets/twitter-icon.svg'); ?>
-                        </div>
-                    </a></li>
-                    <li><a href="https://www.instagram.com/redbrickpaper/">
-                        <div class="icon-container">
-                            <?php echo file_get_contents(get_template_directory() . '/assets/instagram-icon.svg'); ?>
-                        </div>
-                    </a></li>
-                </ul>
-            </div>
-            <a href="#">
-                <?php /** TODO: Make this section admin-configurable */ ?>
-                <div class="latest-issue">
-                    <div class="summary">
-                        <h1>Issue #1500</h1>
-                        <h2>7 December 2018</h2>
+            <div class="upper">
+                <div class="constraint-container">
+                    <div class="social-media">
+                        <h1>Connect</h1>
                         <ul>
-                            <li>Comment: The true meaning of Christmas</li>
-                            <li>Gaming: The best games of the year</li>
+                            <li><a href="https://www.facebook.com/redbrickpaper/">
+                                <div class="icon-container">
+                                    <?php echo file_get_contents(get_template_directory() . '/assets/facebook-icon.svg'); ?>
+                                </div>
+                            </a></li>
+                            <li><a href="https://twitter.com/redbrickpaper">
+                                <div class="icon-container">
+                                    <?php echo file_get_contents(get_template_directory() . '/assets/twitter-icon.svg'); ?>
+                                </div>
+                            </a></li>
+                            <li><a href="https://www.instagram.com/redbrickpaper/">
+                                <div class="icon-container">
+                                    <?php echo file_get_contents(get_template_directory() . '/assets/instagram-icon.svg'); ?>
+                                </div>
+                            </a></li>
                         </ul>
                     </div>
-                    <div class="thumbnail">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/mockups/front-page.jpg"/>
+                </div>
+            </div>
+            
+            <div class="lower">
+                <div class="constraint-container flex-container">
+                    <div class="latest-issue">
+                        <?php /** TODO: Make this section admin-configurable */ ?>
+                        <a href="#">
+                            <div class="summary">
+                                <h1>Issue #1500</h1>
+                                <h2>7 December 2018</h2>
+                                <ul>
+                                    <li>Comment: The true meaning of Christmas</li>
+                                    <li>Gaming: The best games of the year</li>
+                                </ul>
+                            </div>
+                            <div class="thumbnail">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/mockups/front-page.jpg"/>
+                            </div>
+                        </a>
                     </div>
+                    
+                    <?php $redbrick_footer_menu_tree = redbrick_get_nav_menu_items_tree(redbrick_get_nav_menu_object_at_location('redbrick_nav_menu_footer')); ?>
+                    <?php if ($redbrick_footer_menu_tree) : ?>
+                        <div class="footer-links">
+                            <?php echo redbrick_get_html_list_from_menu_tree($redbrick_footer_menu_tree); ?>
+                        </div>
+                    <?php endif; ?>
                 </div>
-            </a>
-            <?php $redbrick_footer_menu_tree = redbrick_get_nav_menu_items_tree(redbrick_get_nav_menu_object_at_location('redbrick_nav_menu_footer')); ?>
-            <?php if ($redbrick_footer_menu_tree) : ?>
-                <div class="footer-links">
-                    <?php echo redbrick_get_html_list_from_menu_tree($redbrick_footer_menu_tree); ?>
-                </div>
-            <?php endif; ?>
+            </div>
         </footer>
         <?php wp_footer(); ?>
     </body>
