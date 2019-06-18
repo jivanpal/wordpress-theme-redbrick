f55e1ddb4648c11dd4d05bdae77653607f4c3f84
Jivan Pal <jivan.pal@gmail.com>
2019-06-18 19:52:17 +0100

    Correctly remove Slider cat from subcat list in order to correctly render no subcat list when no other subcats exist

--- redbrick/category.php
+++ redbrick/category.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
 <?php
-    $category_id    = get_queried_object_id();
-    $category_slug  = get_queried_object()->slug;
+    $redbrick_category_id    = get_queried_object_id();
+    $redbrick_category_slug  = get_queried_object()->slug;
 ?>
 <main class="category">
-    <?php $redbrick_posts = redbrick_get_latest_posts(4, [ 'slider-' . $category_slug ]); ?>
+    <?php $redbrick_posts = redbrick_get_latest_posts(4, [ 'slider-' . $redbrick_category_slug ]); ?>
     <?php if (count($redbrick_posts) != 0): ?>
         <div class="showcase-container">    <?php /** This container is used to apply overflow shadows */ ?>
             <ul class="showcase">
@@ -17,16 +17,21 @@
         </div>
     <?php endif; ?>
 
-    <?php $redbrick_categories = get_categories(['child_of' => $category_id]); ?>
+    <?php
+        $redbrick_categories = get_categories(['child_of' => $redbrick_category_id]);
+        // Remove categories whose slugs begin with `slider-`, if they are present
+        foreach ($redbrick_categories as $redbrick_array_key => $redbrick_category) {
+            if (preg_match('/^slider-/', $redbrick_category->slug) !== 0) {
+                unset($redbrick_categories[$redbrick_array_key]);
+            }
+        }
+    ?>
     <?php if (count($redbrick_categories) != 0): ?>
-        <section class="subsections section--<?php echo $category_slug; ?>">
+        <section class="subsections section--<?php echo $redbrick_category_slug; ?>">
             <ul class="category-list">
                 <?php
                 foreach ($redbrick_categories as $redbrick_category) {
-                    if (preg_match('/^slider-/', $redbrick_category->slug) === 0) {
-                        // If category slug does not beign with `slider-` ...
-                        echo redbrick_get_html_category_item($redbrick_category);
-                    }
+                    echo redbrick_get_html_category_item($redbrick_category);
                 }
                 ?>
             </ul>
@@ -34,9 +39,9 @@
     <?php endif; ?>
 
     <div class="constrained">
-        <?php $redbrick_posts = redbrick_get_latest_posts(12, [ $category_slug ]); ?>
+        <?php $redbrick_posts = redbrick_get_latest_posts(12, [ $redbrick_category_slug ]); ?>
         <?php if (count($redbrick_posts) != 0): ?>
-            <section class="posts section-<?php echo $category_slug; ?>">
+            <section class="posts section-<?php echo $redbrick_category_slug; ?>">
                 <ul class="post-list">
                     <?php 
                     foreach ($redbrick_posts as $redbrick_post) {
